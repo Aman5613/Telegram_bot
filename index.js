27229cdf941e799d1624a93853a895afdce6a9bd
Aman5613 <aman3779956@gmail.com>
2026-01-08 15:40:35 +0530

    final commmit

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
+import { GoogleGenAI } from "@google/genai";
 import { config } from "dotenv";
-config();
-
 import TelegramBot from "node-telegram-bot-api";
+config();
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", (msg) => {
-    console.log(msg);
+const ai = new GoogleGenAI({});
+
+
+const generateReply = async (text) => {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: text,
+    config: {
+      systemInstruction: "You are a telegram bot. Your name is nova.",
+    },
+  });
+  return response.text;
+};
+
+bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Hello! I am your Telegram Bot 🤖");
+  const text = msg.text;
+
+  if (!text) return;
+
+  try {
+    const reply = await generateReply(text);
+
+    bot.sendMessage(chatId, reply);
+
+
+  } catch (error) {
+    bot.sendMessage(
+      chatId,
+      "Sorry, an error occurred while processing your request."
+    );
+    console.error("Error processing message:", error.message || error);
+    return;
+  }
+
 });
 
-// bot.onText(/\/start/, (msg) => {
-//   bot.sendMessage(msg.chat.id, "Welcome! Type /help");
-// });
-
-// bot.onText(/\/help/, (msg) => {
-//   bot.sendMessage(msg.chat.id, "Available commands:\n/start\n/help");
-// });
 
 console.log("Telegram bot is running...");
